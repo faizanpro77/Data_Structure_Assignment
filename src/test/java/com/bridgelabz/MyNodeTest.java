d6829ee10c6164ae7a906b81258d644344116089
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-10 17:40:37 +0530

    MODIFIED: create linked list

--- src/test/java/com/bridgelabz/MyNodeTest.java
+++ src/test/java/com/bridgelabz/MyNodeTest.java
@@ -1,9 +1,20 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyNodeTest {
+    @Test
+    public void givenNumber_whenLinked_returnTrue() {
+        MyNode<Integer> firstNode = new MyNode<>(56);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> thirdNode = new MyNode<>(70);
 
+        firstNode.setNext(secondNode);
+        secondNode.setNext(thirdNode);
+
+        boolean status = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+        Assert.assertTrue(status);
 
     }
 }
