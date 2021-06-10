86a1cf521cb74a673c44ae4eb187f84a1eb30a46
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-10 22:57:28 +0530

    MODIFIED: add element in linked list

--- src/main/java/com/bridgelabz/MyNode.java
+++ src/main/java/com/bridgelabz/MyNode.java
@@ -1,19 +1,28 @@
 package com.bridgelabz;
 
-public class MyNode<E> {
-    private E key;
-    private MyNode<E> next;
+public class MyNode<E> implements INode<E> {
+
+    public E key;
+    public MyNode<E> next;
 
     public MyNode(E key) {
         this.key = key;
         this.next = null;
     }
 
-    public MyNode<E> getNext() {
+    public E getKey() {
+        return key;
+    }
+
+    public void setKey(E key) {
+        this.key = key;
+    }
+
+    public INode<E> getNext() {
         return next;
     }
 
-    public  void  setNext(MyNode<E> next) {
-        this.next = next;
+    public void setNext(INode<E> next) {
+        this.next = (MyNode<E>) next;
     }
 }
